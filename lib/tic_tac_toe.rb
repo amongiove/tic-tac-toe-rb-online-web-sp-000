3402120c7e5a08e623994e7695a03aeaef73c849
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-02 22:49:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,14 +9,15 @@ def display_board(board)
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
+end
 
-  def input_to_index(user_input)
-    user_input.to_i - 1
-  end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
-  def move(board, index, current_player = "X")
-    board[index] = current_player
-  end
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
 
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
