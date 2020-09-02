7059998d0e853461f65a1ad24902de2777556bf5
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-02 22:44:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,6 +45,14 @@ def turn_count(board)
   end
 end
 
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  elsif turn_count(board) % 2 != 0
+    return "O"
+  end
+end
+
 def play(board)
   counter = 0
   until counter == 9
